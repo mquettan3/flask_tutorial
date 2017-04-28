c94442a51a8e8fedb3566ddc13e013830dd899e1
Marcus Quettan <mquettan@gmail.com>
2017-04-27 22:51:00 -0400

    Adding logic to show entries in the repo.  No html template yet but getting there.

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -60,3 +60,12 @@ def initdb_command():
 	init_db()
 	print('Initialized the database.')
 
+#Show entries in database
+@app.route('/')
+def show_entries():
+	db = get_db()
+	cur = db.execute('select title, text from entries order by id desc')
+	entries = cur.fetchall()
+	return render_template('show_entries.html', entries=entries)
+
+
