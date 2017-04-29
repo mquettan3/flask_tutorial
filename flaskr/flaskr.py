ebfa8fb65d3e1c041940d7b96488e421b8006f91
Marcus Quettan <mquettan@gmail.com>
2017-04-29 13:14:30 -0400

    Completed tutorial app.

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -68,4 +68,39 @@ def show_entries():
 	entries = cur.fetchall()
 	return render_template('show_entries.html', entries=entries)
 
+#Add new entry
+#Note: Be sure to use question marks when building SQL statements.  Otherwise, using string formating will cause you to be sucuceptable to SQL injection.
+@app.route('/add', methods=['POST'])
+def add_entry():
+	if not session.get('logged_in'):
+		abort(401)
+	db = get_db()
+	db.execute('insert into entries (title, text) values (?, ?)',
+		[request.form['title'], request.form['text']])
+	db.commit()
+	flash('New entry was successfully posted')
+	return redirect(url_for('show_entries'))
 
+#Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid password'
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return redirect(url_for('show_entries'))
+	return render_template('login.html', error=error)
+
+#Logout
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('show_entries'))
+
+#Note:  Passwords should never be stored in plain text in a production system.  This tutorial uses plain text passwords for simplicity.  If you plan on storing passwords they should be both hashed and salted before being stored in a database or file.  Fortunately, there are flask extensions for this purpose so adding this functionality is fairly straigth forward.
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export FLASK_APP=flaskr
+export FLASK_APP=/home/mquettan3/workspace/flask_tutorials/official_flask_microblog_flaskr/flaskr/flaskr.py
 export FLASK_DEBUG=true
 flask run --host=0.0.0.0
